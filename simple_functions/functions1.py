45835ea7489f32769ce39c65d22791a3680a575c
acse-jat20 <72030772+acse-jat20@users.noreply.github.com>
2020-10-21 13:35:19 +0200

    Update functions1.py

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -12,6 +12,7 @@ def my_sum(iterable):
     return tot
 
 @lru_cache(maxsize=None)  # Note: -> @cache in python >= 3.9
+
 def factorial(number):
     """Calculating a factorial"""
     return number * factorial(number-1) if number else 1
