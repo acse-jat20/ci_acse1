e01fb5e9156c8eb4484f0c374769591a747dcf5b
acse-jat20 <72030772+acse-jat20@users.noreply.github.com>
2020-10-21 14:34:54 +0200

    Solution to exercises

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,7 +1,8 @@
 """The functions to be tested according to lecture 7"""
 
 from functools import lru_cache
-__all__ = ['my_sum', 'factorial']
+from numpy import sqrt
+__all__ = ['my_sum', 'factorial', 'solve_quadratic']
 
 
 def my_sum(iterable):
@@ -16,3 +17,16 @@ def my_sum(iterable):
 def factorial(number):
     """Calculating a factorial"""
     return number * factorial(number-1) if number else 1
+
+
+def solve_quadratic(a, b, c):
+    """Solve a quadratic equation ax^2+bx+c=0 in the reals"""
+    if b**2 - 4.0*a*c > 0:
+        # two solutions
+        return ((-b - sqrt(b**2 - 4.0*a*c))/(2.0*a), (-b + sqrt(b**2 - 4.0*a*c))/(2.0*a))
+    elif b**2 - 4.0*a*c == 0:
+        # one solution
+        return -b/(2.0*a)
+    else:
+        # no solutions
+        return None
--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -4,6 +4,7 @@ import pytest
 
 from simple_functions import my_sum
 from simple_functions import factorial
+from simple_functions import solve_quadratic
 
 
 class TestSimpleFunctions(object):
@@ -26,4 +27,14 @@ class TestSimpleFunctions(object):
     def test_factorial(self, number, expected):
         '''Test our factorial function'''
         answer = factorial(number)
+        assert answer == expected
+
+    @pytest.mark.parametrize('a, b, c, expected', [
+        (1, 0, -1, (-1.0, 1.0)),
+        (1, 0, 0, (0.0)),
+        (1, 0, 1, None)
+    ])
+    def test_quadratic(self, a, b, c, expected):
+        '''Test our factorial function'''
+        answer = solve_quadratic(a, b, c)
         assert answer == expected
